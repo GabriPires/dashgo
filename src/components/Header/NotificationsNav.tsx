5b1f57ab31c862b1c07c38fd1ec0e20b58c8cf05
Gabriel Pires <gabpiresdev@gmail.com>
2022-06-20 19:55:53 -0300

    refactor: header responsivity

--- src/components/Header/NotificationsNav.tsx
+++ src/components/Header/NotificationsNav.tsx
@@ -4,9 +4,9 @@ import { RiNotificationLine, RiUserAddLine } from 'react-icons/ri';
 export const NotificationsNav = () => {
   return (
     <HStack
-      spacing={8}
-      mx={8}
-      pr={8}
+      spacing={{ base: 6, md: 8 }}
+      mx={{ base: 6, md: 8 }}
+      pr={{ base: 6, md: 8 }}
       py={1}
       color={'gray.300'}
       borderRightWidth={1}
