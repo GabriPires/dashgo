d05f325b095c1830a1959e2ab82cfcdf4328cb14
Gabriel Pires <gabpiresdev@gmail.com>
2022-06-20 20:38:30 -0300

    feat: active link with verify inner pages path

--- src/components/Sidebar/NavLink.tsx
+++ src/components/Sidebar/NavLink.tsx
@@ -1,6 +1,6 @@
 import { Icon, Link as ChakraLink, LinkProps, Text } from '@chakra-ui/react';
 import { ElementType } from 'react';
-import Link from 'next/link';
+import { ActiveLink } from '../ActiveLink';
 
 interface NavLinkProps extends LinkProps {
   icon: ElementType;
@@ -10,13 +10,13 @@ interface NavLinkProps extends LinkProps {
 
 export const NavLink = ({ icon, children, href, ...props }: NavLinkProps) => {
   return (
-    <Link href={href} passHref>
+    <ActiveLink href={href} passHref>
       <ChakraLink display={'flex'} alignItems={'center'} {...props}>
         <Icon as={icon} fontSize={20} />
         <Text ml={4} fontWeight={'medium'}>
           {children}
         </Text>
       </ChakraLink>
-    </Link>
+    </ActiveLink>
   );
 };
