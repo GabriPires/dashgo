67d9461ad6553abc6e1686aee1b1b155c08ef10f
Gabriel Pires <gabpiresdev@gmail.com>
2022-06-20 20:11:31 -0300

    refactor: sidebar responsivity with drawer on mobile

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,14 @@
-import { Flex, useBreakpointValue } from '@chakra-ui/react';
+import { Flex, Icon, IconButton, useBreakpointValue } from '@chakra-ui/react';
+import { RiMenuLine } from 'react-icons/ri';
+import { useSidebarDrawer } from '../../contexts/SidebarDrawerContext';
 import { Logo } from './Logo';
 import { NotificationsNav } from './NotificationsNav';
 import { Profile } from './Profile';
 import { SearchBox } from './SearchBox';
 
 export const Header: React.FC = () => {
+  const { onOpen } = useSidebarDrawer();
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
@@ -21,6 +25,19 @@ export const Header: React.FC = () => {
       mt={'4'}
       px={'6'}
     >
+      {!isWideVersion && (
+        <IconButton
+          variant={'unstyled'}
+          aria-label={'Abrir menu'}
+          display={'flex'}
+          alignItems={'center'}
+          fontSize={24}
+          mr={2}
+          icon={<Icon as={RiMenuLine} />}
+          onClick={onOpen}
+        />
+      )}
+
       <Logo />
 
       {isWideVersion && <SearchBox />}
