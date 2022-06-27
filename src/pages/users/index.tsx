96e98d850a2e9bcac58078ca475c26d6fb416d7d
Gabriel Pires <gabpiresdev@gmail.com>
2022-06-27 19:05:23 -0300

    feat: spinner when is fetching users data

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -23,7 +23,7 @@ import Pagination from '../../components/Pagination';
 import { Sidebar } from '../../components/Sidebar';
 
 const UserList = () => {
-  const { data, isLoading, error } = useQuery(
+  const { data, isLoading, isFetching, error } = useQuery(
     'users',
     async () => {
       const response = await fetch('https://localhost:3000/api/users');
@@ -64,6 +64,9 @@ const UserList = () => {
           <Flex mb={8} justify={'space-between'} align={'center'}>
             <Heading size={'lg'} fontWeight={'normal'}>
               Usuários
+              {!isLoading && isFetching && (
+                <Spinner size={'sm'} color={'gray.500'} ml={4} />
+              )}
             </Heading>
             <Link href={'/users/create'} passHref>
               <Button
